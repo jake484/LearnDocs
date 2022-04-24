055b5a3e81b7b71539c3ebe2f2905de3ae23507e
yjy <522432938@qq.com>
2022-04-24 17:31:47 +0800

    updating deploy

---
+++ docs/make.jl
@@ -0,0 +1,40 @@
+using Documenter
+
+makedocs(
+    sitename="LearnDocs",
+    pages=[
+        "Home" => "index.md",
+        "Modeling" => Any[
+            "Modeling/DE_intro.md",
+            "Modeling/MTK_intro.md"
+        ],
+        "Simulation" => Any[
+            "Simulation/componementModel.md",
+            "Simulation/steadyRC.md",
+            "Simulation/heattran.md"
+        ],
+        "Optimization" => Any[
+            "Optimization/参数辨识实例_MTK.md",
+            "Optimization/JuMP参数辨识.md",
+            "Optimization/DE_Estim.md",
+            "Optimization/systemParaEste.md"
+        ],
+        "Control" => Any[
+            "Control/MTKMPC.md",
+            "Control/JuMPMPC.md"
+        ],
+        "CS Base" => Any[
+            "CS Base/env_variable.md",
+            "CS Base/julia_change_pkgserve.md"
+        ],
+        "Tools" => Any[
+            "Tools/CoolProp.md",
+            "Tools/CSV_jl_use.md"
+        ],],
+)
+
+deploydocs(
+   repo="github.com/jake484/LearnDocs.git",
+#    target = "../build",
+   push_preview=true
+)
